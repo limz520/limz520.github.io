9ee40af16722eab4d6247f9f047742eff56ffaa9
limz520 <3036145094@qq.com>
2026-03-20 23:49:27 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "lmz blog",
-	subtitle: "One demo website",
+	subtitle: "lmz的秘密基地",
 	siteURL: "https://limz520.github.io/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2023-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
